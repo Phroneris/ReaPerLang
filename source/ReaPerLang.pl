c5e6fa61071c4b5371fdab13fedb60dacf4241e0
Phroneris <phroneris@gmail.com>
2024-02-16 02:25:22 +0900

    Replace tab with two-column indents and trim trailing spaces programmatically

--- source/ReaPerLang.pl
+++ source/ReaPerLang.pl
@@ -1,34 +1,34 @@
 #!perl
 my $ReaPerLang = 'ReaPerLang v1.12-dev';
 
-use strict  ;		# デバッグ用
-use warnings;		# デバッグ用
-use autodie    ;	# エラー時に$@を得るため
-use Time::HiRes;	# 最後に経過時間を出すため
+use strict  ;   # デバッグ用
+use warnings;   # デバッグ用
+use autodie    ;  # エラー時に$@を得るため
+use Time::HiRes;  # 最後に経過時間を出すため
 
 
 
 ##### 文字エンコーディング関連
 
-use utf8;								# このファイル内に直接書いたUTF-8文字列を全て内部文字列にする
-use open OUT => ':utf8';				# ファイル出力を全て '>:encoding(UTF-8)' で行う
+use utf8;               # このファイル内に直接書いたUTF-8文字列を全て内部文字列にする
+use open OUT => ':utf8';        # ファイル出力を全て '>:encoding(UTF-8)' で行う
 use Encode qw/encode decode/;
 
-my $enc_os = 'cp932';	# Windows JP
-use PerlIO::encoding;	# for building exe
-use Encode::JP;			# for building exe
-use Encode::CN;			# 念のため
-use Encode::KR;			# 念のため
-use Encode::TW;			# 念のため
-binmode STDIN,  ":encoding(${enc_os})";	# 標準入出力で cp932(見た目)⇔UTF-8(内部) と変換する
+my $enc_os = 'cp932'; # Windows JP
+use PerlIO::encoding; # for building exe
+use Encode::JP;     # for building exe
+use Encode::CN;     # 念のため
+use Encode::KR;     # 念のため
+use Encode::TW;     # 念のため
+binmode STDIN,  ":encoding(${enc_os})"; # 標準入出力で cp932(見た目)⇔UTF-8(内部) と変換する
 binmode STDOUT, ":encoding(${enc_os})";
 binmode STDERR, ":encoding(${enc_os})";
 
-sub du($) { decode('UTF-8', shift) };	# 内部文字列にする（文字コードを取り除く）
-sub eu($) { encode('UTF-8', shift) };	# UTF-8にする
+sub du($) { decode('UTF-8', shift) }; # 内部文字列にする（文字コードを取り除く）
+sub eu($) { encode('UTF-8', shift) }; # UTF-8にする
 sub dc($) { decode($enc_os, shift) };
 sub ec($) { encode($enc_os, shift) };
-sub ed($) { ec(du(shift)) };	# デバッグ時にpで文字列が化けたら"ec $var"または"ed $var"で戻せることが多い
+sub ed($) { ec(du(shift)) };  # デバッグ時にpで文字列が化けたら"ec $var"または"ed $var"で戻せることが多い
 # sub isN($) { Encode::is_utf8(shift) ? 'naibu' : 'hadaka kamo...'; }
 
 
@@ -38,12 +38,12 @@ sub ed($) { ec(du(shift)) };	# デバッグ時にpで文字列が化けたら"ec
 my $indent = '';
 sub abort
 {
-	my $err = shift;
-	my $noDecode = shift;
-	$err = dc($err) unless $noDecode;	# エラー文を自前で直接指定する場合、第2引数をtrueにしてデコードを避ける
-	print $indent, '*ERROR*: ', $err, "\n", $indent, 'Press enter to abort.';
-	<STDIN>;
-	exit 1;
+  my $err = shift;
+  my $noDecode = shift;
+  $err = dc($err) unless $noDecode; # エラー文を自前で直接指定する場合、第2引数をtrueにしてデコードを避ける
+  print $indent, '*ERROR*: ', $err, "\n", $indent, 'Press enter to abort.';
+  <STDIN>;
+  exit 1;
 }
 
 my $devmode = 0;
@@ -51,58 +51,58 @@ my $pname;
 my @rfiles = ();
 sub readTxt
 {
-	my ($rname, $rfile, $f);
-	my @default = ('MyLangpack', '00', '01');
-	# @default = ('JPN_Phroneris', '619', '683');
-	my $i = $_[0];
-	if ($devmode==1)
-	{
-		$rname = '';
-		print "\n";
-	}else{
-		chomp($rname = <STDIN>);
-	}
-	$rname = $rname eq '' ? $default[$i] : $rname;
-	my @heads;
-	if ($i == 0)	# 自分の旧LangPackを読む場合
-	{
-		$pname = $rname =~ s/(\.ReaperLangPack)?(\.txt)?$//inr;
-		@heads = ('');
-	}else{			# テンプレートを読み込む場合
-		$rname = "reaper${rname}";
-		@heads = ('', 'template_');
-	}
-	foreach my $head (@heads)
-	{
-		my $found = 0;
-		foreach my $ext ('.ReaperLangPack', '.txt', '.ReaperLangPack.txt', '')
-		{
-			$rfile = "${head}${rname}${ext}";
-			$rfiles[$i] = $rfile;
-			print ' Searching... ', $rfile;
-			if (-f $rfile)
-			{
-				$found = 1;
-				print '  <- Found!', "\n\n";
-				last;
-			}else{
-				print "\n";
-			}
-		}
-		last if $found;
-	}	# 無くてもとりあえずループからは抜ける
-	eval { open $f, '<', ec($rfile) };
-	&abort("Can't find a \"${rname}\"-like file for reading.\n", 1) if $@;	# エラー時
-	my @txt = <$f>;		# 非エラー時
-	close $f;
-	map { $_=du($_) } @txt;
-	map { s/[\x0d\x0a]//g } @txt;	# 改行を削除
-	return @txt;
+  my ($rname, $rfile, $f);
+  my @default = ('MyLangpack', '00', '01');
+  # @default = ('JPN_Phroneris', '619', '683');
+  my $i = $_[0];
+  if ($devmode==1)
+  {
+    $rname = '';
+    print "\n";
+  }else{
+    chomp($rname = <STDIN>);
+  }
+  $rname = $rname eq '' ? $default[$i] : $rname;
+  my @heads;
+  if ($i == 0)  # 自分の旧LangPackを読む場合
+  {
+    $pname = $rname =~ s/(\.ReaperLangPack)?(\.txt)?$//inr;
+    @heads = ('');
+  }else{      # テンプレートを読み込む場合
+    $rname = "reaper${rname}";
+    @heads = ('', 'template_');
+  }
+  foreach my $head (@heads)
+  {
+    my $found = 0;
+    foreach my $ext ('.ReaperLangPack', '.txt', '.ReaperLangPack.txt', '')
+    {
+      $rfile = "${head}${rname}${ext}";
+      $rfiles[$i] = $rfile;
+      print ' Searching... ', $rfile;
+      if (-f $rfile)
+      {
+        $found = 1;
+        print '  <- Found!', "\n\n";
+        last;
+      }else{
+        print "\n";
+      }
+    }
+    last if $found;
+  } # 無くてもとりあえずループからは抜ける
+  eval { open $f, '<', ec($rfile) };
+  &abort("Can't find a \"${rname}\"-like file for reading.\n", 1) if $@;  # エラー時
+  my @txt = <$f>;   # 非エラー時
+  close $f;
+  map { $_=du($_) } @txt;
+  map { s/[\x0d\x0a]//g } @txt; # 改行を削除
+  return @txt;
 }
 sub divDsc
 {
-	my $flg = shift;	# 0なら文書冒頭の概要部以外を、1なら概要部だけを返す
-	return grep { $flg = /^\[common\]/ ? !$flg : $flg } @_;
+  my $flg = shift;  # 0なら文書冒頭の概要部以外を、1なら概要部だけを返す
+  return grep { $flg = /^\[common\]/ ? !$flg : $flg } @_;
 }
 
 
@@ -116,33 +116,33 @@ print <<'EOP';
 
 Mode select (0/1)
 ┃0: First-time mode
-┃1: Repeater mode 
+┃1: Repeater mode
 ┗━━━━━━━━━━
 EOP
 print ' > ';
 my $mode;
 chomp($mode = <STDIN>);
-if ($mode eq 0)	# 英字などの入力のためにeq
+if ($mode eq 0) # 英字などの入力のためにeq
 {
-	print 'First-time mode. Welcome!';
+  print 'First-time mode. Welcome!';
 }
 elsif ($mode eq 1)
 {
-	print 'Repeater mode. Welcome back!';
+  print 'Repeater mode. Welcome back!';
 }
 elsif ($mode eq '0d')
 {
-	print 'Developer mode 0!';
-	$devmode = 1;
-	$mode = 0;
+  print 'Developer mode 0!';
+  $devmode = 1;
+  $mode = 0;
 }
 elsif ($mode eq '1d')
 {
-	print 'Developer mode 1!';
-	$devmode = 1;
-	$mode = 1;
+  print 'Developer mode 1!';
+  $devmode = 1;
+  $mode = 1;
 }else{
-	&abort("Invalid value.\n", 1);
+  &abort("Invalid value.\n", 1);
 }
 
 
@@ -168,13 +168,13 @@ print ' Enter the version in ';
 my @tmpl_old if $mode == 1;
 if ($mode == 1)
 {
-	print 'each template name (the older and current)', "\n";
-	print '  The older: > ';
-	@tmpl_old = &divDsc(0, &readTxt(1));
-	print '  Current: > ';
+  print 'each template name (the older and current)', "\n";
+  print '  The older: > ';
+  @tmpl_old = &divDsc(0, &readTxt(1));
+  print '  Current: > ';
 }else{
-	print 'the current template name:', "\n";
-	print '  > ';
+  print 'the current template name:', "\n";
+  print '  > ';
 }
 my @tmpl_crr;
 @tmpl_crr = &readTxt(2);
@@ -187,22 +187,22 @@ print '  Yes=y, No=n/(blank): > ';
 my $emp_section;
 if ($devmode==1)
 {
-	$emp_section = '';
-	print "\n";
+  $emp_section = '';
+  print "\n";
 }else{
-	chomp($emp_section = <STDIN>);
+  chomp($emp_section = <STDIN>);
 }
-if ($emp_section =~ /^y(?:es)?$/i)		# 大文字/小文字の差は無視
+if ($emp_section =~ /^y(?:es)?$/i)    # 大文字/小文字の差は無視
 {
-	$emp_section = 1;
-	# print ' All-section mode!';
+  $emp_section = 1;
+  # print ' All-section mode!';
 }
-elsif ($emp_section =~ /^(?:no?)?$/i)	# 無記入はNoとする
+elsif ($emp_section =~ /^(?:no?)?$/i) # 無記入はNoとする
 {
-	$emp_section = 0;
-	# print ' No-empty-section mode!';
+  $emp_section = 0;
+  # print ' No-empty-section mode!';
 }else{
-	&abort("Invalid value.\n", 1);
+  &abort("Invalid value.\n", 1);
 }
 print "\n\n";
 
@@ -212,20 +212,20 @@ print "\n\n";
 my @modes = ('First-time', 'Repeater');
 my @yesNo = ('No', 'Yes');
 my @wfiles = (
-	"_${mode}_${rfiles[0]}",
-	"_${mode}_${pname}_missing.txt",
-	"_${mode}_${rfiles[2]}",
-	"_${mode}_${pname}_section.txt"
+  "_${mode}_${rfiles[0]}",
+  "_${mode}_${pname}_missing.txt",
+  "_${mode}_${rfiles[2]}",
+  "_${mode}_${pname}_section.txt"
 );
 my @outInfo = (
-	"┃${modes[$mode]} Mode",
-	"┃in : ${rfiles[0]}",	# 旧言語パック
-	"┃in : ${rfiles[2]}",	# 旧テンプレート
-	"┃out: ${wfiles[0]}",	# 新言語パック
-	"┃out: ${wfiles[1]}",	# missing
-	"┃out: ${wfiles[2]}",	# 新テンプレート
-	"┃Leave empty sections: ${yesNo[$emp_section]}",
-	"┗━━━━━━━━━━━━━━━━━━━━━"
+  "┃${modes[$mode]} Mode",
+  "┃in : ${rfiles[0]}", # 旧言語パック
+  "┃in : ${rfiles[2]}", # 旧テンプレート
+  "┃out: ${wfiles[0]}", # 新言語パック
+  "┃out: ${wfiles[1]}", # missing
+  "┃out: ${wfiles[2]}", # 新テンプレート
+  "┃Leave empty sections: ${yesNo[$emp_section]}",
+  "┗━━━━━━━━━━━━━━━━━━━━━"
 );
 splice @outInfo, 2, 0, "┃in : ${rfiles[1]}" if $mode == 1;
 my @outConfirm = map { ' '.$_."\n" } @outInfo;
@@ -246,13 +246,13 @@ my @tmpl_dsc;
 @tmpl_crr = &divDsc(0, @tmpl_crr);
 if ($#lng_dsc>$#tmpl_dsc)
 {
-	for (my $i=$#tmpl_dsc; $i<$#lng_dsc; $i++) {
-		push @tmpl_dsc, '';
-	}
+  for (my $i=$#tmpl_dsc; $i<$#lng_dsc; $i++) {
+    push @tmpl_dsc, '';
+  }
 }
 elsif ($#lng_dsc<$#tmpl_dsc)
 {
-	splice @tmpl_dsc, $#lng_dsc-1, $#tmpl_dsc, ";/ ... the rest of this description is cut off by $ReaPerLang", '';
+  splice @tmpl_dsc, $#lng_dsc-1, $#tmpl_dsc, ";/ ... the rest of this description is cut off by $ReaPerLang", '';
 }
 
 
@@ -264,18 +264,18 @@ my @lng_missing = ();
 my @section = grep { /^\[/ } @lng_old;
 my $endsec = '[endsec_RPL]';
 push @section, $endsec;
-map { $_=~/^(\[.+?\])(.*)$/; $_=[$1,$2] } @section;		# $section[セクション名][その後のコメント]
+map { $_=~/^(\[.+?\])(.*)$/; $_=[$1,$2] } @section;   # $section[セクション名][その後のコメント]
 push @lng_new, ('', $endsec);
 
-my $Lol = 0;	# @lng_oldの行数
-my $s = -1;		# @sectionの要素数
-my $is_s1st = 1;	# 見つからなかった行がそのセクションで初めてのそれかどうか
-my $Lns_top = 0;	# @tmpl_crrの現在セクション頭の行数
-my $Lns_btm = -1;	# @tmpl_crrの次のセクション頭の行数
+my $Lol = 0;  # @lng_oldの行数
+my $s = -1;   # @sectionの要素数
+my $is_s1st = 1;  # 見つからなかった行がそのセクションで初めてのそれかどうか
+my $Lns_top = 0;  # @tmpl_crrの現在セクション頭の行数
+my $Lns_btm = -1; # @tmpl_crrの次のセクション頭の行数
 sub insertSectionName
 {
-	my $s = shift;
-	push @lng_missing, ('', $section[$s][0].$section[$s][1]);	# "(改行) セクション名+コメント" の形で@lng_missing内に配置
+  my $s = shift;
+  push @lng_missing, ('', $section[$s][0].$section[$s][1]); # "(改行) セクション名+コメント" の形で@lng_missing内に配置
 }
 
 
@@ -283,108 +283,108 @@ sub insertSectionName
 
 my $p_int = 0;
 print ' Processing...', "\n";
-$| = 1;	# オートフラッシュ（printを即出力する）
+$| = 1; # オートフラッシュ（printを即出力する）
 
 foreach my $a (@lng_old)
 {
 
-	my $p_prev = $p_int;
-	my $len = 50;
-	my $progress = $Lol*100/$#lng_old;	# 進行度（実数%）
-	$p_int = int $progress*$len/100;	# 進行度（切捨てバー長さ）
-	printf " %6.2f %% [", $progress;
-	if ($progress == 0)
-	{
-		printf "%s] %5d lines", ' 'x$len, $Lol;
-	}
-	elsif ($p_int != $p_prev)
-	{
-		printf "%s%s] %5d lines", '/'x$p_int, ' 'x($len-$p_int), $Lol;
-	}
-	print "\r";
+  my $p_prev = $p_int;
+  my $len = 50;
+  my $progress = $Lol*100/$#lng_old;  # 進行度（実数%）
+  $p_int = int $progress*$len/100;  # 進行度（切捨てバー長さ）
+  printf " %6.2f %% [", $progress;
+  if ($progress == 0)
+  {
+    printf "%s] %5d lines", ' 'x$len, $Lol;
+  }
+  elsif ($p_int != $p_prev)
+  {
+    printf "%s%s] %5d lines", '/'x$p_int, ' 'x($len-$p_int), $Lol;
+  }
+  print "\r";
 
-	my $Lnw = 0;	# @lng_newの行数
-	my $hit = 0;	# 一致した奴ありましたよ判定(0/1)
-	# $DB::single=1 if $Lol>=11;
-	
-	if ($a =~ /^\[/)	# セクション名の行ならば
-	{
-		$s++;
-		$is_s1st = 1;
-		&insertSectionName($s) if $emp_section==1;
-		$Lnw = $Lns_btm + 1;
-		while ($hit<1 and $Lnw<=$#lng_new)
-		{
-			if ($lng_new[$Lnw] =~ /^\Q$section[$s][0]/)
-			{
-				$lng_new[$Lnw]=$a;		# @lng_new（≒@tmpl_crr）にセクション名を記載（セクションコメント込み）
-				$Lns_top = $Lnw;		# @lng_newを読む時の範囲を同名セクション内のみに抑制（上限）
-				$hit = -1;
-			}
-			elsif ($hit == -1 and $lng_new[$Lnw] =~ /^\[/)
-			{
-				$Lns_btm = $Lnw - 1;	# @lng_newを読む時の範囲を同名セクション内のみに抑制（下限）
-				$hit = 1;
-			}
-			$Lnw++;
-		}
-	}
-	elsif ($a =~ /^5CA1E00000000000=/)	# スケール情報の行ならば
-	{
-		splice @lng_new, $Lns_btm, 0, $a;	# @lng_newの同一セクションの末尾にスケール情報を追加
-		my $scaled = '5CA1E...........=*scaled*';
-		splice @tmpl_crr, $Lns_btm, 0, $scaled;				# その分、@tmpl_crrの行数を@lng_newに合わせる
-		splice @tmpl_old, $Lol, 0, $scaled if $mode == 1;	# 同様に、@tmpl_oldの行数を@lng_oldに合わせる
-		$Lns_btm++;
-	}
-	elsif ($a =~ /^((?:;\/\^?)?)(\w{16})=(.*)/)	# スケール情報ではない、翻訳済み行または意図的な無効化行ならば
-	{
-		my $oo_a = $1;	# ;/ または ;/^ の意図的な無効化行（opt-out、「（森）」の独自記法）
-		my $code_a = $2;
-		my $str_a = $3;
-		my $yet_init = ';';	# 未翻訳接頭辞
-		if (($mode==0 and $oo_a=~/;\/\^/) or ($mode==1 and $tmpl_old[$Lol]=~/^;\^/))
-		{
-			$yet_init = ';^';
-		}
-		
-		$Lnw = $Lns_top;
-		$hit = 0;
-		while ($hit==0 and $Lnw<=$Lns_btm)	# @lng_newを頭から（または属するセクション内のみ）読む
-		{
-			if ($lng_new[$Lnw] =~ /^\Q${yet_init}\E${code_a}=/)	# 未翻訳接頭辞付きのコード一致行ならば
-			{
-				$lng_new[$Lnw] = $a;
-				$hit = 1;
-				# eval { print "${Lol}: ${a}\n"; };
-				# if ($@) {
-					# print '*ERROR* :', dc($@);
-					# $DB::single = 1;
-				# }
-			}
-			$Lnw++;
-		}
-		if ($hit == 0)		# @lng_oldと@tmpl_crrでコード一致の行が全くないならば
-		{
-			if ($emp_section==0 and $is_s1st==1)
-			{
-				&insertSectionName($s);
-				$is_s1st = 0;
-			}
-			if ($mode == 1)
-			{
-				my $str_sub = $tmpl_old[$Lol] =~ s/^;\^?//r;
-				$a =~ s/^(?:;\/\^?)?//;
-				$yet_init = $oo_a if $oo_a;	# 接頭辞指定。意図的な無効化行ならそれを、そうでないならオプション行区別のために元のを。
-				push @lng_missing, $yet_init.$str_sub, $yet_init.$a;
-			}else{
-				push @lng_missing, $a;
-			}
-		}
-		$Lol = $Lol + 0;
-	}
+  my $Lnw = 0;  # @lng_newの行数
+  my $hit = 0;  # 一致した奴ありましたよ判定(0/1)
+  # $DB::single=1 if $Lol>=11;
 
-	$Lol++;
+  if ($a =~ /^\[/)  # セクション名の行ならば
+  {
+    $s++;
+    $is_s1st = 1;
+    &insertSectionName($s) if $emp_section==1;
+    $Lnw = $Lns_btm + 1;
+    while ($hit<1 and $Lnw<=$#lng_new)
+    {
+      if ($lng_new[$Lnw] =~ /^\Q$section[$s][0]/)
+      {
+        $lng_new[$Lnw]=$a;    # @lng_new（≒@tmpl_crr）にセクション名を記載（セクションコメント込み）
+        $Lns_top = $Lnw;    # @lng_newを読む時の範囲を同名セクション内のみに抑制（上限）
+        $hit = -1;
+      }
+      elsif ($hit == -1 and $lng_new[$Lnw] =~ /^\[/)
+      {
+        $Lns_btm = $Lnw - 1;  # @lng_newを読む時の範囲を同名セクション内のみに抑制（下限）
+        $hit = 1;
+      }
+      $Lnw++;
+    }
+  }
+  elsif ($a =~ /^5CA1E00000000000=/)  # スケール情報の行ならば
+  {
+    splice @lng_new, $Lns_btm, 0, $a; # @lng_newの同一セクションの末尾にスケール情報を追加
+    my $scaled = '5CA1E...........=*scaled*';
+    splice @tmpl_crr, $Lns_btm, 0, $scaled;       # その分、@tmpl_crrの行数を@lng_newに合わせる
+    splice @tmpl_old, $Lol, 0, $scaled if $mode == 1; # 同様に、@tmpl_oldの行数を@lng_oldに合わせる
+    $Lns_btm++;
+  }
+  elsif ($a =~ /^((?:;\/\^?)?)(\w{16})=(.*)/) # スケール情報ではない、翻訳済み行または意図的な無効化行ならば
+  {
+    my $oo_a = $1;  # ;/ または ;/^ の意図的な無効化行（opt-out、「（森）」の独自記法）
+    my $code_a = $2;
+    my $str_a = $3;
+    my $yet_init = ';'; # 未翻訳接頭辞
+    if (($mode==0 and $oo_a=~/;\/\^/) or ($mode==1 and $tmpl_old[$Lol]=~/^;\^/))
+    {
+      $yet_init = ';^';
+    }
+
+    $Lnw = $Lns_top;
+    $hit = 0;
+    while ($hit==0 and $Lnw<=$Lns_btm)  # @lng_newを頭から（または属するセクション内のみ）読む
+    {
+      if ($lng_new[$Lnw] =~ /^\Q${yet_init}\E${code_a}=/) # 未翻訳接頭辞付きのコード一致行ならば
+      {
+        $lng_new[$Lnw] = $a;
+        $hit = 1;
+        # eval { print "${Lol}: ${a}\n"; };
+        # if ($@) {
+          # print '*ERROR* :', dc($@);
+          # $DB::single = 1;
+        # }
+      }
+      $Lnw++;
+    }
+    if ($hit == 0)    # @lng_oldと@tmpl_crrでコード一致の行が全くないならば
+    {
+      if ($emp_section==0 and $is_s1st==1)
+      {
+        &insertSectionName($s);
+        $is_s1st = 0;
+      }
+      if ($mode == 1)
+      {
+        my $str_sub = $tmpl_old[$Lol] =~ s/^;\^?//r;
+        $a =~ s/^(?:;\/\^?)?//;
+        $yet_init = $oo_a if $oo_a; # 接頭辞指定。意図的な無効化行ならそれを、そうでないならオプション行区別のために元のを。
+        push @lng_missing, $yet_init.$str_sub, $yet_init.$a;
+      }else{
+        push @lng_missing, $a;
+      }
+    }
+    $Lol = $Lol + 0;
+  }
+
+  $Lol++;
 
 }
 
@@ -398,8 +398,8 @@ print "\n\n", ' Writing...', "\n\n\n";
 
 my $date = localtime;
 my @additionalInfo = (
-	"┃Date: ${date} (local)",
-	"┃Time: ${time} sec"
+  "┃Date: ${date} (local)",
+  "┃Time: ${time} sec"
 );
 splice @outInfo, -1, 0, @additionalInfo;
 unshift @lng_missing, "Generated by ${ReaPerLang}", @outInfo;
@@ -411,16 +411,16 @@ unshift @tmpl_crr, @tmpl_dsc;
 
 sub writeTxt
 {
-	my $wfile = $wfiles[shift];
-	my @txt = @_;
-	my $f;
+  my $wfile = $wfiles[shift];
+  my @txt = @_;
+  my $f;
 
-	map { $_=$_."\n" } @txt;
-	eval { open $f, '>', ec($wfile) };
-	&abort($@) if $@;	# エラー時
-	print $f @txt;		# 非エラー時
-	close $f;
-	print ' Completed writing "', $wfile, '": about ', $#txt, " lines.\n";
+  map { $_=$_."\n" } @txt;
+  eval { open $f, '>', ec($wfile) };
+  &abort($@) if $@; # エラー時
+  print $f @txt;    # 非エラー時
+  close $f;
+  print ' Completed writing "', $wfile, '": about ', $#txt, " lines.\n";
 }
 
 &writeTxt(0, @lng_new);
